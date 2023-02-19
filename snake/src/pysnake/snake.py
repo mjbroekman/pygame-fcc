fdf4a5eba960984839fab15fab956fcc7f4e13ca
Maarten Broekman <maarten@broekman.us>
2023-02-18 19:01:36 -0500

    poison snacks, aging food, and a good end screen...

--- snake/src/pysnake/snake.py
+++ snake/src/pysnake/snake.py
@@ -63,6 +63,10 @@ class Snake(object):
     def alive(self) -> bool:
         return self._alive
     
+    @property
+    def size(self) -> int:
+        return len(self.body)
+
     @alive.setter
     def alive(self,state:bool):
         self._alive = state
@@ -169,7 +173,7 @@ class Snake(object):
         self.body.append(Segment(start=p_segment.get_old_pos(),cell_x=self._cellx,cell_y=self._celly,color=self._color))
 
 class Board(object):
-    def __init__(self,width,height,rows,cols,bgcolor,fgcolor,skcolor,debug):
+    def __init__(self,width,height,rows,cols,bgcolor,fgcolor,debug):
         _real_width = ( width // cols ) * cols
         _real_height = ( height // rows ) * rows
         self.width = _real_width
@@ -178,7 +182,6 @@ class Board(object):
         self.cols = cols
         self._bgcolor = bgcolor
         self._fgcolor = fgcolor
-        self._skcolor = skcolor
         self.debug = debug
         self._window = pygame.display.set_mode((self.width+1,self.height+1))
         pygame.display.update()
@@ -248,66 +251,155 @@ class Board(object):
         self._window.fill(self._bgcolor) # Fill the surface with the background color
         self.drawGrid()
         for snack in snacklist:
-            pygame.draw.rect(self._window,self._skcolor,Rect(snack[0]*self.col_width,snack[1]*self.row_height,self.col_width,self.row_height))
+            snack.draw(self._window,self.col_width,self.row_height)
+
+
+class Snack(object):
+    _poisonous = (200,250,15)
+
+    def __init__(self,pos,skcolor,poison=False):
+        (self._col,self._row) = pos
+        self._poison = poison
+        self.color = self._poisonous if self.poison else skcolor
+        self._age = 0
+
+    @property
+    def position(self):
+        return (self._col,self._row)
+
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self,incr):
+        self._age += incr
+        self.color = tuple(int(a + (b - a) * (self._age/25)) for a, b in zip(self.color, self._poisonous))
+        if self.color == self._poisonous:
+            self._poison = True
+
+    @property
+    def poison(self):
+        return self._poison
+
+    def draw(self,surface,width,height):
+        pygame.draw.rect(surface,self.color,Rect(self._col*width,self._row*height,width,height))
 
 
 class Game():
     _snacks = []
 
-    def __init__(self,width,height,rows,cols,bgcolor,sncolor,hdcolor,skcolor,fgcolor,debug):
+    def __init__(self,width,height,rows,cols,bgcolor,sncolor,hdcolor,skcolor,fgcolor,max_snacks,debug):
         pygame.init()
         pygame.display.set_caption('Snake Game')
-        self.window = Board(width,height,rows,cols,bgcolor,fgcolor,skcolor,debug)
+        self.window = Board(width,height,rows,cols,bgcolor,fgcolor,debug)
         pygame.display.update()
         self.snake = Snake(sncolor,hdcolor,(rows // 2,cols // 2),width // cols,height // rows,debug) # Color is an RGB tuple
         self.clock = pygame.time.Clock()
         self.debug = debug
+        self.snack_color = skcolor
+        self.bgcolor = bgcolor
         self.delay = 100
+        self.max_snacks = max_snacks
 
-    def randomSnack(self,snake_cells,num_snacks=1):
+    def randomSnack(self,snake):
         #                              X                                  Y
         new_snack = (random.randrange(0,self.window.cols),random.randrange(0,self.window.cols))
-        while len(self._snacks) < num_snacks:
-            if new_snack not in snake_cells and new_snack not in self._snacks:
+        if len(self._snacks) < self.max_snacks:
+            if new_snack not in snake.get_snake_pos() and new_snack not in self._snacks:
                 if self.debug > 0:
                     print("Attempting to add a snack... at {}".format(new_snack))
-                self._snacks.append(new_snack)
+
+                poison = False
+                if random.randint(0,(rows*cols)) < self.snake.size:
+                    poison = True
+
+                self._snacks.append(Snack((new_snack),self.snack_color,poison))
 
             new_snack = (random.randrange(0,self.window.cols),random.randrange(0,self.window.cols))
 
-
     def message_box(self,subject,content):
-        pass
+        pygame.display.set_caption("Game Over")
+        surface = self.window.get_surface()
+        font = pygame.font.Font('freesansbold.ttf', 24)
+        top_msg = font.render(subject,True,"white","black")
+        mid_msg = font.render(content,True,"white","black")
+        end_msg = font.render("Press ESC to quit",True,"white","black")
+        msgBox1 = top_msg.get_rect()
+        msgBox2 = mid_msg.get_rect()
+        msgBox3 = end_msg.get_rect()
+        msgBox1.center = (self.window.width // 2, self.window.height // 4)
+        msgBox2.center = (self.window.width // 2, self.window.height // 2)
+        msgBox3.center = (self.window.width // 2, (self.window.height // 4) * 3)
+        print(subject)
+        print(content)
+
+        while True:
+            surface.fill(self.bgcolor)
+            surface.blit(top_msg,msgBox1)
+            surface.blit(mid_msg,msgBox2)
+            surface.blit(end_msg,msgBox3)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                keys = pygame.key.get_pressed()
+                if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+                    pygame.quit()
+                    sys.exit()
 
     def play(self):
+        _moves = 0
+        _score = 0
         while self.snake.alive:
+            _moves += 1
             self.snake.alive = self.snake.move(self.window.rows,self.window.cols)
             self.snake.draw(self.window.get_surface())
-            self.randomSnack(self.snake.get_snake_pos(),num_snacks=1)
+
+            # 10% change to generate a snack...
+            if random.randint(1,100) < 10:
+                self.randomSnack(self.snake)
+
             for snack in self._snacks:
                 if self.debug > 0:
                     print("Snack at {}  || Snake Head at {}".format(snack,self.snake.get_head_pos()))
-                if snack == self.snake.get_head_pos():
+                if snack.position == self.snake.get_head_pos():
+                    if snack.poison:
+                        self.snake.alive = False
+                        self.endgame("You ate a poison snack and died!",score=_score)
+
                     self.snake.addSegment()
                     self._snacks.remove(snack)
+                    _score += snack.age // 2
+
+                elif _moves % 75 and random.randint(0,100) > 50:
+                    snack.age = 1
+
+                if random.randint(self.window.rows,(self.window.rows*self.window.cols)) < snack.age:
+                    self._snacks.remove(snack)
+
+            # Reduce game delay (speed up the game) 
+            if random.randint(0,100) > (100 - self.snake.size) and self.delay > 10:
+                if self.debug > 3:
+                    print("Faster! Hahahahahahaha!")
+                self.delay -= 1
 
             pygame.display.update()
+
             self.window.redrawWindow(self._snacks)
             pygame.time.delay(self.delay) # delay between loop activity aka game 'tick'
             self.clock.tick(1000)    # Sets game refresh to 10 frames per second
 
         _snake_head = self.snake.get_head_pos()
         if _snake_head in self.snake.get_snake_pos()[1:]:
-            self.endgame("We became an ouroboros and ate our own body.")
+            self.endgame("You are an ouroboros.",score=_score)
         
-        self.endgame()
+        self.endgame("Exiting the game",score=_score)
 
-    def endgame(self,msg=""):
-        score = len(self.snake.get_snake_pos()) - 1 # Number of _additional_ segments in the snake
-        end_msg = f"{msg}\nFinal Score: {score}"
-        
-        pygame.quit()
-        sys.exit()
+    def endgame(self,msg="",score=-1):
+        bonus = 100 - self.delay  # Give bonus points for the faster you were moving
+        final_score = score + bonus
+        end_msg = f"Final Score: {final_score} (Life: {score}, Bonus: {bonus})"
+        self.message_box(msg,end_msg)
 
 
 
@@ -321,6 +413,7 @@ sncolor = (255,0,0) # Make the snake red. This should be an RGB tuple.
 hdcolor = (255,0,0) # Make the head of the snake red. This should be an RGB tuple.
 skcolor = (0,255,0) # Make the snacks green. This should be an RGB tuple
 fgcolor = (255,255,255) # Make the foreground white. These are the lines. This should be an RGB tuple
+max_snack = 1
 
 if __name__ == '__main__':
     # Use argparse to read command-line arguments to create the board...
@@ -336,6 +429,7 @@ if __name__ == '__main__':
     parser.add_argument("--head",action="store",type=str,help="Color of snake head",default="red")
     parser.add_argument("--fgcolor",action="store",type=str,help="Foreground color",default="white")
     parser.add_argument("--debug","-d",action="count",help="Set debug level. Can be used multiple times to increase debug level",default=0)
+    parser.add_argument("--maxsnack",action="store",type=int,help="Maximum number of simultaneous snacks",default=1)
     args = parser.parse_args()
 
     try:
@@ -372,11 +466,12 @@ if __name__ == '__main__':
         if args.debug > 0:
             print(f"Unknown color: '{args.fgcolor}'. Defaulting to 'white'")
         _fgcolor = fgcolor
-    
+
+    max_snack = args.maxsnack    
     width = args.width
     height = args.height
     rows = args.rows
     cols = args.cols
 
-    game = Game(width,height,rows,cols,_bgcolor,_snake,_head,_snack,_fgcolor,args.debug)
+    game = Game(width,height,rows,cols,_bgcolor,_snake,_head,_snack,_fgcolor,max_snack,args.debug)
     game.play()
